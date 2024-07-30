be7456e9303631bc45e4d0c6f517f38c3eaa6dbe
Bayurzx <bayurzx@gmail.com>
2024-07-30 17:44:06 +0100

    Pushing to github

--- src/components/health-data-form.tsx
+++ src/components/health-data-form.tsx
@@ -208,10 +208,10 @@ export default function HealthDataForm() {
 
               <div className="border-b border-gray-900/10 pb-12 dark:border-gray-100">
                 <h2 className="text-base font-semibold leading-7 text-gray-900 dark:text-gray-100">
-                  Personal Information
+                  Health Data
                 </h2>
                 <p className="mt-1 text-sm leading-6 text-gray-600 dark:text-gray-400">
-                  Use a permanent address where you can receive mail. Use a permanent address where you can receive mail.
+                You can view your health information from your profile.
 
                 </p>
 
